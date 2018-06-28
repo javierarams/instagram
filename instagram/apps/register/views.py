e3041b2f8996c0a8325169f727d70ede1a83b24d
Javier Aramberri <>
2018-06-27 21:20:51 -0300

    no message

--- instagram/apps/register/views.py
+++ instagram/apps/register/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from apps.register.forms import RegisterForm
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'register/index.html')
+
+def register_view(request):
+	if request.method == 'POST':
+		form = RegisterForm(request.POST)
+		if form.is_valid():
+				form.save()
+		return redirect('register:index')
+	else:
+		form = RegisterForm()
+
+	return render(request, 'register/register_form.html', {'form':form})
