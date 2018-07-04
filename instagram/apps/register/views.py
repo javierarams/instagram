0b1fdde7e44fa6273938c0f3c79fade1ec2709b3
Rocio Aramberri <rocio@opencraft.com>
2018-07-03 17:02:21 -0700

    Fix

--- instagram/apps/register/views.py
+++ instagram/apps/register/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import PasswordChangeForm
 from django.views.decorators.csrf import csrf_exempt
 
-from apps.register.forms import RegisterForm, UserForm, EditRegisterForm, EditProfileForm
+from apps.register.forms import RegisterForm, UserForm
 from apps.register.models import UserProfile
 
 
